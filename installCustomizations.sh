4d487e7762056172d4a2967d8a84a3475a88b560
Tom Gehrke <tomgehrke@gmail.com>
2022-02-16 01:12:14 +0000

    Fleshing out

--- installCustomizations.sh
+++ installCustomizations.sh
@@ -2,35 +2,63 @@
 # Install Desktop Customizations
 
 # PREPARATION --------------------
+
+echo Adding sources...
+echo =============================================
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo rm microsoft.gpg
+
+echo Refresh updates...
+echo =============================================
 sudo apt update
 
-# Install Git
-sudo apt install git
-
-# Install Latte Dock
-sudo apt install latte-dock
+echo Install packages...
+echo =============================================
+sudo apt install -y fonts-tlwg-garuda-ttf
+sudo apt install -y apt-transport-https
+sudo apt install -y code
+sudo apt install -y microsoft-edge-stable
+sudo apt install -y latte-dock
+sudo apt install -y qt5-style-kvantum qt5-style-kvantum-themes
 
 # DOWNLOADS ----------------------
 
-# Download Nordic GTK Theme
-git clone https://github.com/EliverLara/Nordic.git ~/Nordic
-# Move to system themes folder
-sudo mv ~/Nordic/ /usr/share/themes/
+echo Download and install Nordic theme...
+echo ==============================================
+git clone https://github.com/EliverLara/Nordic ~/Nordic
+cp -a ~/Nordic/kde/* ~/.local/share
+mkdir -p ~/.config/Kvantum
+mv ~/.local/share/kvantum/Nordic* ~/.config/Kvantum/
+mv ~/.local/share/colorschemes ~/.local/share/color-schemes
+mkdir -p ~/.local/share/aurorae/themes # Window Decorations
+mv ~/.local/share/aurorae/Nordic* ~/.local/share/aurorae/themes/
 
-# Download and run Papirus installation script
+echo Download and install Papirus icons...
+echo ===============================================
 wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/install.sh | sh
+
+echo Download and install Caskaydia Cove NerdFont...
+echo ===============================================
+wget -qO- https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip | unzip -qo -d ~/.fonts
+fc-cache -fv
 
 # INSTALLATION -------------------
 
 # Enable theme
-lookandfeeltool --apply Nordic
+lookandfeeltool --apply Nordic-darker
 
-# CLEANUP ------------------------
+# FIXES --------------------------
 # ToDo: check for existence of lines first
 
-# Fix login screen on hiDPI display
+echo Fix login screen on hiDPI display...
+echo ===============================================
 sudo echo -e "xrandr --output eDP-1 --mode 1920x1080 --rate 60.01" >> /usr/share/sddm/scripts/Xsetup
 
-# Fix GRUB menu on hiDPI display
+echo Fix GRUB menu on hiDPI display...
+echo ===============================================
 sudo echo -e "GRUB_GFXPAYLOAD=keep\nGRUB_TERMINAL=gfxterm\nGRUB_GFXMODE=800x600" >> /etc/default/grub 
 sudo update-grub 
+
